9642c0312f948bdcc321c2032c30798621f06bde
Giorgos Karantonis <gkaranto@redhat.com>
2020-07-09 21:09:17 -0400

    bug fix

--- preprocessing.py
+++ preprocessing.py
@@ -5,11 +5,7 @@ from nltk import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
-try:
-	from transformers import BertTokenizer, TFBertForSequenceClassification
-except:
-	!pip install transformers==3.0.0
-	from transformers import BertTokenizer, TFBertForSequenceClassification
+from transformers import BertTokenizer, TFBertForSequenceClassification
 
 
 
