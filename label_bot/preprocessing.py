f958221f0f8fcba43356349909cc4dbd535aebb7
Giorgos Karantonis <gkaranto@redhat.com>
2020-08-20 16:30:01 -0400

    updated license

--- label_bot/preprocessing.py
+++ label_bot/preprocessing.py
@@ -1,5 +1,5 @@
 # Github-Issues-Classifier
-# Copyright(C) 2018, 2019, 2020 Georgios (Giorgos) Karantonis
+# Copyright(C) 2020 Georgios (Giorgos) Karantonis
 #
 # This program is free software: you can redistribute it and / or modify
 # it under the terms of the GNU General Public License as published by
